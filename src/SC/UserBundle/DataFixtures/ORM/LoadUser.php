6eebd4a8cf3d4ff5c04061e6f27a94793adce297
fadio78 <f.doumbia24@yahoo.fr>
2015-05-27 14:00:37 +0200

    peut se connecter

--- src/SC/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/SC/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -30,7 +30,7 @@ class LoadUser implements FixtureInterface
       
      
       // On ne se sert pas du sel pour l'instant
-     /* $user->setSalt(''); */
+      $user->setSalt(''); 
       // On définit uniquement le role ROLE_USER qui est le role de base
      // $user->setRoles(array('ROLE_USER'));
 
