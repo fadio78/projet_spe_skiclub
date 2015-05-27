6f5b94d62d2161dfb22704a762cea1a6b9e47700
floydmoney <slayer57.nc@gmail.com>
2015-05-27 11:24:46 +0200

    fichier permettant d'ajouter des User a la base

---
+++ src/SC/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace SC\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use SC\UserBundle\Entity\User;
+
+class LoadUser implements FixtureInterface
+{
+  public function load(ObjectManager $manager)
+  {
+    // Les noms d'utilisateurs à créer
+    $listNames = array('sgbd@hotmail.com', 'test@hotmail.com', 'sfr@hotmail.com');
+
+    foreach ($listNames as $name) {
+      // On crée l'utilisateur
+      $user = new User;
+
+      // Le nom d'utilisateur et le mot de passe sont identiques
+      $user->setEmail($name);
+      $user->setNom('monNom');
+      $user->setPrenom('monPrenom');
+      $user->setAdresse('14 rue grenoble');
+      $user->setCommune('grenoble');
+      $user->setTelephone('06125566333');
+      $user->setPassword('azerty');
+      $user->setType('ok');
+      $user->setValidite(1);
+      
+      // On ne se sert pas du sel pour l'instant
+      $user->setSalt('');
+      // On définit uniquement le role ROLE_USER qui est le role de base
+      $user->setRoles(array('ROLE_USER'));
+
+      // On le persiste
+      $manager->persist($user);
+    }
+
+    // On déclenche l'enregistrement
+    $manager->flush();
+  }
+}
+
