dc426204e3b2c77d030d5ffe8149f0b23fbea8ff
floydmoney <slayer57.nc@gmail.com>
2015-05-27 14:45:46 +0200

    ajout de commentaires

--- src/SC/UserBundle/DataFixtures/ORM/LoadEnfant.php
+++ src/SC/UserBundle/DataFixtures/ORM/LoadEnfant.php
@@ -14,8 +14,10 @@ class LoadEnfant extends AbstractFixture implements OrderedFixtureInterface
 {
   public function load(ObjectManager $manager)
   {
+    //on recupere tous les user de la BD  
     $advertRepository = $manager->getRepository('SC\UserBundle\Entity\User')->findAll();
     
+    //on va creer un nouvel enfant
     foreach ($advertRepository as $client) {
         $enfant = new Enfant;
         $enfant->setDateNaissance('17aout'); 
@@ -24,40 +26,12 @@ class LoadEnfant extends AbstractFixture implements OrderedFixtureInterface
         $enfant->setPrenomEnfant('monPrenom');
         $enfant->setUserParent($client);
         
+        //ajout dans la base
         $manager->persist($enfant);
     }
-    /*
-        $user = new User;
-            
-        $user->setEmail('supp@hotmail.com');
-        $user->setNom('monNom');
-        $user->setPrenom('monPrenom');
-        $user->setAdresse('14 rue grenoble');
-        $user->setCommune('grenoble');
-        $user->setTelephone('06125566333');
-        $user->setPassword('azerty');
-        $user->setType('ok');
-        $user->setValidite(1);
-      
-        // On ne se sert pas du sel pour l'instant
-        $user->setSalt('');
-        // On définit uniquement le role ROLE_USER qui est le role de base
-        $user->setRoles(array('ROLE_USER'));      
-
-        
-        $enfant = new Enfant;
-        $enfant->setDateNaissance('17aout'); 
-        $enfant->setNiveauSki('bon'); 
-        $enfant->setNomEnfant('monNom'); 
-        $enfant->setPrenomEnfant('monPrenom');
-        $enfant->setUserParent($user);
-
-        $manager->persist($user);
-        $manager->persist($enfant);*/
-        
-
         $manager->flush(); 
   }
+  
   public function getOrder()
   {
         return 2; // l'ordre dans lequel les fichiers sont chargés
