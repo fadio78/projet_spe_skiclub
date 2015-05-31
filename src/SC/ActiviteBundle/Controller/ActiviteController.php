12e678cbe67a8623b51f437670aa16ee311b6baf
raniaberrazaga <raniaberrazaga@gmail.com>
2015-05-31 23:03:56 +0200

    inscri activite

--- src/SC/ActiviteBundle/Controller/ActiviteController.php
+++ src/SC/ActiviteBundle/Controller/ActiviteController.php
@@ -244,57 +244,5 @@ class ActiviteController extends Controller
         }
     }
     
-    
-    public function inscriptionActiviteAction($id,Request $request) 
-    {
-        $em = $this->getDoctrine()->getManager();
-        $session = $request->getSession();
-        $email = $session->get('email');
-        $inscriptionActivite = new InscriptionActivite();
-        $activite = $em->getRepository('SC\ActiviteBundle\Entity\Activite')->find($id);
-        $inscriptionActivite -> setActivite($activite);
-        $inscriptionActivite -> setPrixPayeActivite(0);
-        $inscriptionActivite -> setEmail($email);
-        $year = $this->connaitreSaison();  
-        $saison = $em->getRepository('SC\ActiviteBundle\Entity\Saison')->find($year);
-        unset($season);
-        $inscriptionActivite -> setSaison($saison);
-        $defaultData = array('message' => 'Type your message here');
-        $form = $this->createFormBuilder($defaultData)
-           ->add('Enfant', 'entity', array('class'=> 'SC\UserBundle\Entity\Enfant','property' => 'prenomNom', 'multiple' => false,'expanded' => false,'required' => true, 'query_builder' => function (EnfantRepository $repository) use ($email) { return $repository->
-            getEnfant($email); },))
-          ->add('enregistrer','submit')
-          ->getForm();
-        $form->handleRequest($request);
-        if ($form->isValid()){
-            $data = $form->getData();
-            $enfant = $data ['Enfant'];
-            $inscriptionActivite ->setNomEnfant($enfant -> getNomEnfant());
-            $inscriptionActivite ->setPrenomEnfant($enfant -> getPrenomEnfant());
-          
-            $er = $em ->getRepository('SC\ActiviteBundle\Entity\InscriptionActivite');
-            $inscri = $er->Est_inscrit($inscriptionActivite);
-            if ($inscri == null)
-            {
-              
-                $em->persist($inscriptionActivite);
-                $em->flush();
-                $request->getSession()->getFlashBag()->add('info', 'Inscription bien enregistrée');
-                return $this->redirect($this->generateUrl('sc_activite_homepage'));
-            }
-          
-            else 
-            {
-                $request->getSession()->getFlashBag()->add('info', 'Enfant déjà inscrit');
-                return $this->render('SCActiviteBundle:InscriptionActivite:addinscriactivite.html.twig', array(
-            'form' => $form->createView(),
-            ));
-           }
-       }
-            return $this->render('SCActiviteBundle:InscriptionActivite:addinscriactivite.html.twig', array(
-            'form' => $form->createView(),
-            ));
-    
-    }
 
 }
