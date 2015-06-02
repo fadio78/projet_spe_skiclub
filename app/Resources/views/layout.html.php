415182df71732793ac30842189158e6b68164f38
floydmoney <slayer57.nc@gmail.com>
2015-06-02 15:30:45 +0200

    nouveeau fichier pour le html.php

---
+++ app/Resources/views/layout.html.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>SkiClub des petites rocheuses</title>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+ 
+</head>
+
+<body>
+  <div class="container">
+    <div id="header" class="jumbotron">
+      <h1>SkiClub des petites rocheuses</h1>
+      <p>
+        Ce projet est propulsé par Symfony2,
+        et construit par les élèves de Ensimag.
+      </p>
+      <p>
+        <a class="btn btn-primary btn-lg" href="https://scpetitesroches.wordpress.com/">
+          Voir l'ancien site web»
+        </a>
+      </p>
+    </div>
+
+    <div class="row">
+      <div id="menu" class="col-md-3">
+        <h3>SKiClub</h3>
+
+        
+
+      </div>
+      <div id="content" class="col-md-9">
+
+      </div>
+    </div>
+
+    <hr>
+
+    <footer>
+      <p>The sky's the limit </p>
+    </footer>
+  </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
