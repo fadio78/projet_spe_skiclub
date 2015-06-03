148f9af0de3095381e225cc08729cb6e4362ebe9
floydmoney <slayer57.nc@gmail.com>
2015-06-03 17:08:37 +0200

    modif vue

--- src/SC/ActiviteBundle/Resources/views/Activite/tableauRecap.html.php
+++ src/SC/ActiviteBundle/Resources/views/Activite/tableauRecap.html.php
@@ -8,31 +8,51 @@
      <center/>
      <h2>Activite : <?php echo $nomAct?></h2>
      <h2>Saison : <?php echo $year?></h2>
-     <br/><br/>
+
 <ul>     
 <?php
-    foreach ($sorties as $sortie) { ?>
-     <li> <?php  echo $sortie->getDateSortie().' '.$sortie->getLieu()->getNomLieu();?> </li>
+    foreach ($sorties as $sortie) { ?>      <br/><br/>
+ 
+  <li>   <?php  echo $sortie->getDateSortie().' '.$sortie->getLieu()->getNomLieu();?> </li>
      
-     <br/>
 <?php     
         $inscrits = $em->getRepository('SC\ActiviteBundle\Entity\InscriptionSortie')
                         ->findBy(array('dateSortie'=>$sortie->getDateSortie(),'idActivite'=>$id,'saison'=>$year,'lieu'=>$sortie->getLieu()->getNomLieu()));
 ?>   <?php      
         if(empty($inscrits)) { ?> 
-       <ul> <li>  <?php  echo 'aucun enfant inscrit'; ?></li> </ul>
-        <?php } ?>
-     
+        <br/>
+       <center> <b> <?php  echo 'aucun enfant inscrit'; ?></b> </center>
+        <?php } else {?>
+  <table border="1" cellpadding="10" cellspacing="1" width="50%">
+        <tr> 
+            <th><center>Nom </center></th>
+            <th><center>Prenom </center></th>
+            <th><center>Confirmé</center></th>
+        <tr/> 
+       
         <?php
+
         foreach($inscrits as $inscrit) { ?>
-        <ul>  <li> <?php echo $inscrit->getNomEnfant().' '.$inscrit->getParticipation().' '.$inscrit->getEmailParent(); ?> </li> </ul>
- <br/>
+       
+     
+
+            <tr>     
+                <td><center> <?php echo $inscrit->getNomEnfant(); ?>  </center></td>  
+                <td><center>  <?php echo $inscrit->getPrenomEnfant(); ?>   </center></td>  
+                
+                    <?php if ($inscrit->getParticipation() == 1) {?> 
+                       <td width=5  bgcolor="#01DF01"><center></center></td>
+                    <?php } else { ?> 
+                        <td width=5  bgcolor="#DF0101"><center></center></td>
+                  <?php } ?> 
+            </tr> 
+       
 <?php
         }
 ?>  
-<br/> <br/>
+<br/> <br/> </table>
 <?php
-    }                    
+        } }                    
 ?>  
 </ul>
     </BODY>
